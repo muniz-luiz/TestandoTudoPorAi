b37d692efd147af17773145a9667a14e2b6fba33
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-02-13 08:59:23 -0300

    Adicionando um novo conhecimento de override 'Vitual e override'

--- TestandoPorAi/Entities/SavingsAccount.cs
+++ TestandoPorAi/Entities/SavingsAccount.cs
@@ -23,6 +23,11 @@ namespace TestandoPorAi.Entities
         {
             Balance += Balance * InterestRate;
         }
+
+        public override void Withdraw(double amount)
+        {
+            Balance += amount;
+        }
     }
 
 
